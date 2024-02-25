fd43066b3463b674306eef3bc20f0e15be373cb1
alumber-creator <alumbersuper@gmail.com>
2024-02-25 12:28:56 +0500

    wip

---
+++ src/main/java/me/alumber/clockme/ClockMe.java
@@ -0,0 +1,17 @@
+package me.alumber.clockme;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClockMe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
